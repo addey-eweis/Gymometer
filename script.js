e404cb61e821c59cbfc348f31386cd54c1186867
Addey Eweis <addey@Addeys-MacBook-Pro-2.local>
2023-05-28 18:35:14 +0300

    Fixed Tab Features + Quotes

--- script.js
+++ script.js
@@ -1,73 +1,16 @@
-const cardBackground = document.getElementById("workout");
-const cardsContainer = document.getElementById("workoutsGrid").offsetHeight;
-const workoutGrid = document.getElementById("workoutsGrid");
+const cards = document.getElementsByClassName("card");
+function selectTab(tabIndex) {
+   
+   for (i = 0; i < cards.length; i++){
+      cards[i].style.display = "none";
+   }
 
-
-cardBackground.style.height = `${cardBackground.offsetHeight + cardsContainer}px`;
-workoutGrid.style.visibility = "hidden";
-
-if (workoutGrid.style.visibility === "hidden") {
-   cardsContainer.style.height = "0"
-   cardBackground.style.height = "20vh"
+   for (i = 0; i < document.getElementsByClassName(`tab${tabIndex}`).length; i++){
+      const element = document.getElementsByClassName(`tab${tabIndex}`)
+      element[i].style.display = "block";
+      document.getElementById(`button${tabIndex}`).focus();
+   }
 }
-
-
-
-
-
-
-
-
-document.getElementById("chest").addEventListener("click", function () {
-   let cards = document.getElementsByClassName("card");
-   workoutGrid.style.visibility = "visible";
-   for (var i = 0; i < cards.length; i++) {
-      cards[i].textContent = `he`;
-   }
- });
-
-document.getElementById("back").addEventListener("click", function () {
-   let cards = document.getElementsByClassName("card");
-   for (var i = 0; i < cards.length; i++) {
-      cards[i].textContent = `${document.getElementById()}`;
-   }
- });
-
- document.getElementById("biceps").addEventListener("click", function() {
-    let cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-       cards[i].textContent = `${document.getElementById()}`;
-    }
- });
-
-document.getElementById("triceps").addEventListener("click", function () {
-    let cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-       cards[i].textContent = `${document.getElementById()}`;
-    }
-});
-document.getElementById("shoulders").addEventListener("click", function () {
-    let cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-       cards[i].textContent = `${document.getElementById()}`;
-    }
-});
-
-document.getElementById("forearms").addEventListener("click", function () {
-    let cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-       cards[i].textContent = `${document.getElementById()}`;
-    }
-});
-
-document.getElementById("legs").addEventListener("click", function () {
-    let cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-       cards[i].textContent = `${document.getElementById()}`;
-    }
-});
- 
-
 
 
 
